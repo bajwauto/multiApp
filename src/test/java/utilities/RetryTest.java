8f73239015d44bca9ebf32e8a7189b84670775b8
Gurman Singh <gurman5691@gmail.com>
2020-08-21 07:37:22 +0530

    Created custom annotation - Retry. Created class TestAnnotationTransformer to automatically set the retryAnalyzer class to the newly created class RetryTest

---
+++ src/test/java/utilities/RetryTest.java
@@ -0,0 +1,30 @@
+package utilities;
+
+import static utilities.Log.info;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import utilities.customAnnotations.Retry;
+
+public class RetryTest implements IRetryAnalyzer {
+	private ThreadLocal<Integer> timesRetried = new ThreadLocal<Integer>() {
+		@Override
+		protected Integer initialValue() {
+			return 0;
+		}
+	};
+
+	@Override
+	public boolean retry(ITestResult result) {
+		Retry retry = result.getMethod().getConstructorOrMethod().getMethod().getAnnotation(Retry.class);
+		System.out.println(timesRetried.get() + " -- " + retry.value());
+		if (retry != null && timesRetried.get() < retry.value()) {
+			timesRetried.set(timesRetried.get() + 1);
+			info("Retrying(Attempt " + timesRetried.get() + ") the test \"" + result.getMethod().getMethodName()
+					+ "\" with the parameters \"" + result.getParameters()[0] + "\"");
+			return true;
+		} else
+			return false;
+	}
+}
