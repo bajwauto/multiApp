1ef369f05a941b9fab19576467417128bb939ff9
Gurman Singh <gurman5691@gmail.com>
2020-08-21 07:35:27 +0530

    Added assertions

--- src/test/java/com/goibibo/Flights.java
+++ src/test/java/com/goibibo/Flights.java
@@ -1,56 +1,75 @@
 package com.goibibo;
 
+import static utilities.Log.error;
+
 import java.util.Map;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.goibibo.pages.FlightsPage;
 import com.goibibo.pages.HomePage;
 
+import utilities.customAnnotations.Retry;
+
 public class Flights extends Base {
+	@Retry
 	@Test(dataProvider = "excel", priority = 1, enabled = true)
 	public void oneWayFlightSearch(Map<String, Object> testData) throws Exception {
-		String sourceCity = (String) testData.get("sourceCity");
-		String destinationCity = (String) testData.get("destinationCity");
-		String departureDate = (String) testData.get("departureDate");
-		int adults = ((Double) testData.get("adultCount")).intValue();
-		int children = ((Double) testData.get("childCount")).intValue();
-		int infants = ((Double) testData.get("infantCount")).intValue();
-		String travelClass = (String) testData.get("class");
-		HomePage homePage = new HomePage();
-		FlightsPage flightsPage = homePage.topMenu.openFlightsMenu();
-		flightsPage.selectTripType("One way");
-		flightsPage.selectSourceCity(sourceCity);
-		flightsPage.selectDestinationCity(destinationCity);
-		flightsPage.setDepartureDate(departureDate, "d-M-yy");
-		flightsPage.setAdultTravellers(adults);
-		flightsPage.setChildTravellers(children);
-		flightsPage.setInfantTravellers(infants);
-		flightsPage.selectTravelClass(travelClass);
-		flightsPage.searchFlights();
+		try {
+			String sourceCity = (String) testData.get("sourceCity");
+			String destinationCity = (String) testData.get("destinationCity");
+			String departureDate = (String) testData.get("departureDate");
+			int adults = ((Double) testData.get("adultCount")).intValue();
+			int children = ((Double) testData.get("childCount")).intValue();
+			int infants = ((Double) testData.get("infantCount")).intValue();
+			String travelClass = (String) testData.get("class");
+			HomePage homePage = new HomePage();
+			FlightsPage flightsPage = homePage.topMenu.openFlightsMenu();
+			flightsPage.selectTripType("One way");
+			flightsPage.selectSourceCity(sourceCity);
+			flightsPage.selectDestinationCity(destinationCity);
+			flightsPage.setDepartureDate(departureDate, "d-M-yy");
+			flightsPage.setAdultTravellers(adults);
+			flightsPage.setChildTravellers(children);
+			flightsPage.setInfantTravellers(infants);
+			flightsPage.selectTravelClass(travelClass);
+			flightsPage.searchFlights();
+		} catch (Exception e) {
+			e.printStackTrace();
+			error(e.getMessage());
+			Assert.fail(e.getMessage(), e);
+		}
 	}
 
+	@Retry
 	@Test(dataProvider = "excel", priority = 2, enabled = true)
 	public void roundTripFlightSearch(Map<String, Object> testData) throws Exception {
-		String sourceCity = (String) testData.get("sourceCity");
-		String destinationCity = (String) testData.get("destinationCity");
-		String departureDate = (String) testData.get("departureDate");
-		String returnDate = (String) testData.get("returnDate");
-		int adults = ((Double) testData.get("adultCount")).intValue();
-		int children = ((Double) testData.get("childCount")).intValue();
-		int infants = ((Double) testData.get("infantCount")).intValue();
-		String travelClass = (String) testData.get("class");
-		HomePage homePage = new HomePage();
-		FlightsPage flightsPage = homePage.topMenu.openFlightsMenu();
-		flightsPage.selectTripType("Round Trip");
-		flightsPage.selectSourceCity(sourceCity);
-		flightsPage.selectDestinationCity(destinationCity);
-		flightsPage.setDepartureDate(departureDate, "d-M-yy");
-		flightsPage.setReturnDate(returnDate, "d-M-yy");
-		flightsPage.setAdultTravellers(adults);
-		flightsPage.setChildTravellers(children);
-		flightsPage.setInfantTravellers(infants);
-		flightsPage.selectTravelClass(travelClass);
-		flightsPage.searchFlights();
+		try {
+			String sourceCity = (String) testData.get("sourceCity");
+			String destinationCity = (String) testData.get("destinationCity");
+			String departureDate = (String) testData.get("departureDate");
+			String returnDate = (String) testData.get("returnDate");
+			int adults = ((Double) testData.get("adultCount")).intValue();
+			int children = ((Double) testData.get("childCount")).intValue();
+			int infants = ((Double) testData.get("infantCount")).intValue();
+			String travelClass = (String) testData.get("class");
+			HomePage homePage = new HomePage();
+			FlightsPage flightsPage = homePage.topMenu.openFlightsMenu();
+			flightsPage.selectTripType("Round Trip");
+			flightsPage.selectSourceCity(sourceCity);
+			flightsPage.selectDestinationCity(destinationCity);
+			flightsPage.setDepartureDate(departureDate, "d-M-yy");
+			flightsPage.setReturnDate(returnDate, "d-M-yy");
+			flightsPage.setAdultTravellers(adults);
+			flightsPage.setChildTravellers(children);
+			flightsPage.setInfantTravellers(infants);
+			flightsPage.selectTravelClass(travelClass);
+			flightsPage.searchFlights();
+		} catch (Exception e) {
+			e.printStackTrace();
+			error(e.getMessage());
+			Assert.fail(e.getMessage(), e);
+		}
 	}
 }
